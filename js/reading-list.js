a05030aa95e4d68921d925cae02a7bed8f23a892
Brandon Amos <bamos@cs.cmu.edu>
2015-04-08 08:16:20 -0400

    Code formatting.

--- js/reading-list.js
+++ js/reading-list.js
@@ -8,59 +8,59 @@ var finished_yaml = null;
 var compilations_yaml = null;
 
 Handlebars.registerHelper('for', function(from, to, block) {
-  var accum = '';
-  for(var i = from; i < to; ++i) {
-    accum += block.fn(i);
-  }
-  return accum;
+    var accum = '';
+    for(var i = from; i < to; ++i) {
+        accum += block.fn(i);
+    }
+    return accum;
 });
 Handlebars.registerHelper('escape', function(variable) {
-  return variable.replace(/(['"])/g, '\\$1');
+    return variable.replace(/(['"])/g, '\\$1');
 });
 function loadYAML(name, f) {
-  var client = new XMLHttpRequest();
-  client.open('GET', 'data/' + name + '.yaml');
-  client.onreadystatechange = function() {
-    var yaml = jsyaml.load(client.responseText);
-    if (yaml) {
-      f(yaml);
+    var client = new XMLHttpRequest();
+    client.open('GET', 'data/' + name + '.yaml');
+    client.onreadystatechange = function() {
+        var yaml = jsyaml.load(client.responseText);
+        if (yaml) {
+            f(yaml);
+        }
     }
-  }
-  client.send();
+    client.send();
 }
 function loadLists() {
-  loadYAML("to-read", function(yaml) {
-    yaml.sort(function(a,b) {
-      return a.author.localeCompare(b.author);
+    loadYAML("to-read", function(yaml) {
+        yaml.sort(function(a,b) {
+            return a.author.localeCompare(b.author);
+        });
+        $("#to-read").html(to_read_tmpl(yaml));
+        to_read_yaml = yaml;
     });
-    $("#to-read").html(to_read_tmpl(yaml));
-    to_read_yaml = yaml;
-  });
-  loadYAML("finished", function(yaml) {
-    yaml.sort(function(a,b) {
-      if (a.finished > b.finished) return -1;
-      if (a.finished < b.finished) return 1;
-      return 0;
+    loadYAML("finished", function(yaml) {
+        yaml.sort(function(a,b) {
+            if (a.finished > b.finished) return -1;
+            if (a.finished < b.finished) return 1;
+            return 0;
+        });
+        function dateString(d) {
+            return d.getFullYear().toString() + "/" +
+                (d.getMonth() + 1).toString() + "/" +
+                (d.getDate() + 1).toString();
+        }
+        var len = yaml.length;
+        for (var i = 0; i < len; i++) {
+            yaml[i].finished = dateString(yaml[i].finished);
+        }
+        $("#finished").html(finished_tmpl(yaml));
+        finished_yaml = yaml;
     });
-    function dateString(d) {
-      return d.getFullYear().toString() + "/" +
-        (d.getMonth() + 1).toString() + "/" +
-        (d.getDate() + 1).toString();
-    }
-    var len = yaml.length;
-    for (var i = 0; i < len; i++) {
-      yaml[i].finished = dateString(yaml[i].finished);
-    }
-    $("#finished").html(finished_tmpl(yaml));
-    finished_yaml = yaml;
-  });
-  loadYAML("compilations", function(yaml) {
-    yaml.sort(function(a,b) {
-      return a.title.localeCompare(b.title);
+    loadYAML("compilations", function(yaml) {
+        yaml.sort(function(a,b) {
+            return a.title.localeCompare(b.title);
+        });
+        $("#compilations").html(comp_tmpl(yaml));
+        compilations_yaml = yaml;
     });
-    $("#compilations").html(comp_tmpl(yaml));
-    compilations_yaml = yaml;
-  });
 }
 
 function showModal(title,yaml,idx) {
@@ -82,7 +82,7 @@ $(document).on('click', '.bootbox', function(){
 
 try{ clicky.init(100602499); }catch(e){}
 try {
-  var snowplowTracker = Snowplow.getTrackerUrl('derecho.elijah.cs.cmu.edu:8080');
-  snowplowTracker.enableLinkTracking();
-  snowplowTracker.trackPageView();
+    var snowplowTracker = Snowplow.getTrackerUrl('derecho.elijah.cs.cmu.edu:8080');
+    snowplowTracker.enableLinkTracking();
+    snowplowTracker.trackPageView();
 } catch (err) {}
