62fced7283271bc959d86b38b2653d22f1f5d54c
Sean Oldham <seanosaur@me.com>
2015-05-19 18:24:39 -0500

    add +1 back to UTCMonth
    With the +1 gone from month, the entire date shifts down a month. Adding the +1 back fixes it.

--- js/reading-list.js
+++ js/reading-list.js
@@ -48,7 +48,7 @@ function loadLists() {
             });
             function dateString(d) {
                 return d.getUTCFullYear().toString() + "/" +
-                    (d.getUTCMonth()).toString() + "/" +
+                    (d.getUTCMonth() + 1).toString() + "/" +
                     (d.getUTCDate()).toString();
             }
             var len = yaml.length;
